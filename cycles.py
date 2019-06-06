88c0f61682244ecbbeedf6900d4d561f60a642a8
Rafael Ferreira da Silva <rafsilva@isi.edu>
2019-06-06 11:18:03 -0400

    adding support for cycles baseline runs

--- cycles.py
+++ cycles.py
@@ -12,12 +12,14 @@ __IO_TYPES__ = {
     "cycles_crops": "CyclesCrops",
     "cycles_ctrl": "CyclesCtrl",
     "cycles_operation": "CyclesOperation",
+    "cycles_ctrl1": "CyclesCtrl",
+    "cycles_operation1": "CyclesOperation",
     "cycles_soil": "CyclesSoil",
     "cycles_weather": "CyclesWeather",
 }
 
 
-def _process_operation_file(kwargs, input_folder_dir):
+def _process_operation_file(kwargs, input_folder_dir, baseline=False):
     with open('cycles/templates/template.operation') as t_op_file:
         src = Template(t_op_file.read())
         op_data = {
@@ -28,14 +30,15 @@ def _process_operation_file(kwargs, input_folder_dir):
             'tillage_date':  int(kwargs['start_planting_date']) + 20
         }
         result = src.substitute(op_data)
-        op_filename = Path(input_folder_dir + '/' + kwargs['unique_id'] + '.operation')
+        f = 'baseline_' + kwargs['unique_id'] if baseline else kwargs['unique_id']
+        op_filename = Path(input_folder_dir + '/' + f + '.operation')
         with op_filename.open('w') as op_file:
             op_file.write(result)
             return op_filename
     return None
 
 
-def _process_ctrl_file(kwargs, input_folder_dir, op_filename):
+def _process_ctrl_file(kwargs, input_folder_dir, op_filename, baseline=False):
     with open('cycles/templates/template.ctrl') as t_ctrl_file:
         src = Template(t_ctrl_file.read())
         ctrl_data = {
@@ -44,11 +47,13 @@ def _process_ctrl_file(kwargs, input_folder_dir, op_filename):
             'crop_file': 'crops.crop',
             'operation_file': op_filename,
             'soil_file': kwargs['soil'],
-            'weather_file': kwargs['weather']
+            'weather_file': kwargs['weather'],
+            'reinit': 0 if baseline else 1
         }
         result = src.substitute(ctrl_data)
 
-        ctrl_filename = Path(input_folder_dir + '/' + kwargs['unique_id'] + '.ctrl')
+        f = 'baseline_' + kwargs['unique_id'] if baseline else kwargs['unique_id']
+        ctrl_filename = Path(input_folder_dir + '/' + f + '.ctrl')
         with ctrl_filename.open('w') as ctrl_file:
             ctrl_file.write(result)
             return ctrl_filename
@@ -60,19 +65,26 @@ def process_input(kwargs):
     if not os.path.exists(input_folder_dir):
         os.makedirs(input_folder_dir)
 
-    # Cycles operation file
-    op_filename = _process_operation_file(kwargs, input_folder_dir)
+    # baseline args
+    baseline_kwargs = kwargs.copy()
+    baseline_kwargs['start_planting_date'] = 100
+    baseline_kwargs['fertilizer_rate'] = 156.25
+    baseline_op_filename = _process_operation_file(baseline_kwargs, input_folder_dir, True)
+    baseline_ctrl_filename = _process_ctrl_file(baseline_kwargs, input_folder_dir, baseline_op_filename.name, True)
 
-    # Cycles control file
+    # Main simulation
+    op_filename = _process_operation_file(kwargs, input_folder_dir)
     ctrl_filename = _process_ctrl_file(kwargs, input_folder_dir, op_filename.name)
 
     if kwargs['crop'] != 'Maize':
         return None
 
     return {
+        "cycles_ctrl": baseline_ctrl_filename,
+        "cycles_operation": baseline_op_filename,
         "cycles_crops": "file:crops.crop",
-        "cycles_ctrl": ctrl_filename,
-        "cycles_operation": op_filename,
+        "cycles_ctrl1": ctrl_filename,
+        "cycles_operation1": op_filename,
         "cycles_soil": "file:pongo.soil",
         "cycles_weather": "file:" + kwargs['weather'],
         "unique_id": kwargs['unique_id'],
