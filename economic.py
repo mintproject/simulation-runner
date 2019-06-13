5acb14f0d01c7e76a8ffb9ebd0db245079014f42
Rajiv Mayani <mayani@isi.edu>
2019-06-12 19:23:29 -0700

    Add more methods to make model more concrete.

--- economic.py
+++ economic.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Economic Model."""
 
+import os
 from pathlib import Path
 
 __WINGS_TEMPLATE_NAME__ = "economic-v5-standalone"
@@ -18,12 +19,56 @@ __IO_TYPES__ = {
 
 
 def process_input(kwargs):
+    # Create input directory
+    input_dir = "./economic/inputs/"
+    if not os.path.exists(input_dir):
+        os.makedirs(input_dir)
+
     return {
-        "cycles-data": "file:cyclesdata.csv",
-        "land-input": "file:landinput.csv",
-        "price": "file:price.csv",
-        "production-cost": "file:productioncost.csv",
-        "sim-price": "file:simprice.csv",
-        "sim-production-cost": "file:simproductioncost.csv",
-        "supply-elasticity": "file:supplyelasticity.csv",
+        "cycles-data": _generate_cycles_data(**kwargs),
+        "land-input": _generate_land_input(**kwargs),
+        "price": _generate_price(**kwargs),
+        "production-cost": _generate_production_cost(**kwargs),
+        "sim-price": _generate_sim_price(**kwargs),
+        "sim-production-cost": _generate_sim_production_cost(**kwargs),
+        "supply-elasticity": _generate_supply_elasticity(**kwargs),
     }
+
+
+def _generate_cycles_data(**kwargs):
+    return "file:cyclesdata.csv"
+
+
+def _generate_land_input(**kwargs):
+    return "file:landinput.csv"
+
+
+def _generate_price(**kwargs):
+    return "file:price.csv"
+
+
+def _generate_production_cost(**kwargs):
+    return "file:productioncost.csv"
+
+
+def _generate_sim_price(**kwargs):
+    o = Path("./economic/inputs/" + kwargs["unique_id"])
+    with o.open("w") as f:
+        f.write(
+            """,p
+cassava,0.26064
+groundnuts,0.80474
+maize,0.30263
+sesame,0.74735
+sorghum,0.3742
+"""
+        )
+    return o
+
+
+def _generate_sim_production_cost(**kwargs):
+    return "file:simproductioncost.csv"
+
+
+def _generate_supply_elasticity(**kwargs):
+    return "file:supplyelasticity.csv"
