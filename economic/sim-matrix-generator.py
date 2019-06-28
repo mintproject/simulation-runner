ce7cb74f26ce055894cc605ee6effb99342de537
Rajiv Mayani <mayani@isi.edu>
2019-06-28 14:01:45 -0700

    Add price, production_cost vars to sim matrix

--- economic/sim-matrix-generator.py
+++ economic/sim-matrix-generator.py
@@ -3,31 +3,49 @@
 
 import csv
 import random
-from itertools import *
+from itertools import product
 
 column_names = [
     "unique_id",
     "disabled",
     "country",
     "crop",
-    "sim-price",
-    "sim-production-c1",
-    "sim-production-c2",
+    "price",
+    "production_cost_c1",
+    "production_cost_c2",
+    "sim_price",
+    "sim_production_c1",
+    "sim_production_c2",
     "notes",
 ]
 
 
 country = ["South Sudan"]
 disabled = ["False"]
-crop = ["cassava,groundnuts,maize,sesame,sorghum"]
+crop = ["cassava", "groundnuts", "maize", "sesame", "sorghum"]
+
+price = [0.26064, 0.80474, 0.30263, 0.74735, 0.3742]
 sim_price = range(-50, 50, 10)
+
+production_cost_c1 = [383, 383, 175, 383, 350]
 sim_production_c1 = range(-50, 50, 10)
+
+production_cost_c2 = [2.5, 2.5, 1, 2.5, 2.7]
 sim_production_c2 = range(-50, 50, 10)
 notes = [" "]
 
 rows = list(
     product(
-        disabled, country, crop, sim_price, sim_production_c1, sim_production_c2, notes
+        disabled,
+        country,
+        [",".join(crop)],
+        [",".join([str(x) for x in price])],
+        [",".join([str(x) for x in production_cost_c1])],
+        [",".join([str(x) for x in production_cost_c2])],
+        sim_price,
+        sim_production_c1,
+        sim_production_c2,
+        notes,
     )
 )
 
