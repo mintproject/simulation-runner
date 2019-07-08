1a603efbd551b3c9355cefd8b04c0f2395f69e11
Rafael Ferreira da Silva <rafsilva@isi.edu>
2019-07-08 15:27:35 -0700

    updating cycles output parser

--- cycles/cycles-graphs.R
+++ cycles/cycles-graphs.R
@@ -1,11 +1,41 @@
 library(ggplot2)
 library(scales)
 
-c <- data.frame(read.csv("/Users/rafsilva/Downloads/mint-penn-state-2/simulation-runner/cycles-output-summary.csv"))
-c <- subset(c, year=='2017')
+c <- data.frame(read.csv("/Users/rafsilva/Downloads/mint-penn-state-2/simulation-runner/cycles/output-summary.csv"))
+d <- subset(c, year=='2017')
+
+### Nitrogen Rate and Weed Fraction for a specific year
+pdf(file="/Users/rafsilva/Downloads/cycles-nr-year-2017.pdf", height=5)
+ggplot(d, aes(x=nitrogen_rate, y=yield, color=as.character(weed_fraction), linetype=as.character(weed_fraction))) + 
+  geom_smooth() +
+  xlab('Nitrogen Rate') +
+  ylab('Grain Yield (Mg/ha)') +
+  facet_wrap(~ location, ncol=4) +
+  theme_light() +
+  scale_color_hue('Weed Fraction') +
+  scale_linetype_manual('Weed Fraction', values=c(2,3,4,5,6)) +
+  theme(legend.position='bottom')
+dev.off()
+
+### Planting Dates and Weed Fraction
+### Only analyzing fixed planting dates
+d <- subset(d, planting_date_fixed=='True')
+d <- subset(d, location=='8.88 North x 27.12 East')
+
+ggplot(d, aes(x=planting_date, y=yield, color=as.character(weed_fraction))) + 
+  geom_smooth() +
+  xlab('Planting Day') +
+  ylab('Grain Yield (Mg/ha)') +
+  facet_wrap(~ nitrogen_rate, ncol=3) +
+  scale_color_hue('Weed Fraction') +
+  theme_light()
+
+
+
+#### OLD PLOTS
 
 pdf(file="/Users/rafsilva/Downloads/cycles-pd-year.pdf", height=5)
-ggplot(c, aes(x=planting_date, y=yield)) + 
+ggplot(d, aes(x=planting_date, y=yield)) + 
   geom_point(aes(shape=as.character(nitrogen_rate), color=as.character(nitrogen_rate))) + 
   geom_smooth() +
   xlab('Planting Day') +
@@ -19,7 +49,7 @@ ggplot(c, aes(x=planting_date, y=yield)) +
 dev.off()
 
 pdf(file="/Users/rafsilva/Downloads/cycles-n-year.pdf", height=5)
-ggplot(c, aes(x=nitrogen_rate, y=yield)) + 
+ggplot(d, aes(x=nitrogen_rate, y=yield)) + 
   geom_point(aes(shape=as.character(planting_date), color=as.character(planting_date))) + 
   geom_smooth() +
   xlab('Nitrogen Rate') +
@@ -31,7 +61,7 @@ ggplot(c, aes(x=nitrogen_rate, y=yield)) +
   scale_y_sqrt()
 dev.off()
 
-ggplot(c, aes(x=nitrogen_rate, y=yield)) + 
+ggplot(d, aes(x=nitrogen_rate, y=yield)) + 
   geom_point(aes(shape=as.character(planting_date), color=as.character(planting_date))) + 
   geom_smooth(aes(color=location)) +
   xlab('Nitrogen Rate') +
--- cycles/cycles-output-parser.py
+++ cycles/cycles-output-parser.py
@@ -5,27 +5,30 @@ import argparse
 import csv
 import os
 
-def parse_outputs(output_folder, simulation_matrix, **kwargs):
+def parse_outputs(output_folder, simulation_matrix, crop_name, **kwargs):
     sim_matrix = {}
     with open(simulation_matrix) as sm:
         reader = csv.reader(sm, skipinitialspace=True, quotechar="'")
         for row in reader:
             sim_matrix[row[0]] = row[1:]
 
-    with open('cycles-output-summary.csv', 'w', newline='') as csvfile:
+    with open('output-summary.csv', 'w', newline='') as csvfile:
         csvwriter = csv.writer(csvfile)
         csvwriter.writerow([
             'unique_id',
             'crop',
             'location',
             'planting_date',
+            'planting_date_fixed',
             'nitrogen_rate',
+            'weed_fraction',
             'year',
-            'yield'
+            'yield',
+            'forcing'
         ])
 
         for d in os.listdir(output_folder):
-            if d.startswith('cycles-'):
+            if os.path.isdir(output_folder + '/' + d) and d.startswith('cycles-' + crop_name.lower()) and 'fertilizer_increase' not in d:
                 with open(output_folder + '/' + d + '/output/' + d + '/season.dat') as season_file:
                     csvreader = csv.reader(season_file, delimiter='\t')
                     next(csvreader)
@@ -36,9 +39,12 @@ def parse_outputs(output_folder, simulation_matrix, **kwargs):
                             sim_matrix[d][1],
                             sim_matrix[d][4].replace('.weather', '').replace('met', '').replace('x', ' x ').replace('N', ' North').replace('E', ' East'),
                             sim_matrix[d][6],
+                            sim_matrix[d][12],
                             sim_matrix[d][9],
+                            sim_matrix[d][13],
                             row[0][:4],
-                            row[4]
+                            row[4],
+                            sim_matrix[d][11]
                             ])
 
 
@@ -48,6 +54,7 @@ def _main():
     )
     parser.add_argument("output_folder", help="Cycles outputs folder")
     parser.add_argument("simulation_matrix", help="Cycles simulation matrix")
+    parser.add_argument("crop_name", help="Cycles crop name")
     args = parser.parse_args()
     parse_outputs(**vars(args))
 
